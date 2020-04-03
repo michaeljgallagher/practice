e66e953b16709b35eca0777c3a0f71ed8f5d9e29
MG <11604739+michaeljgallagher@users.noreply.github.com>
2020-04-02 21:34:32 -0400

    minimum_distances

--- minimum_distances.py
+++ minimum_distances.py
@@ -1,3 +1,9 @@
+'''
+We define the distance between two array values as the number of indices between
+the two values. Given a, find the minimum distance between any pair of equal
+elements in the array. If no such value exists, print -1.
+'''
+
 def minimum_distances(a):
     d = {}
     n = len(a)
